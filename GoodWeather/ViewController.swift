f02407783308ea2bfad507db2908bfbbfc36135e
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2019-12-31 22:07:01 +0900

    Add text field and label

--- GoodWeather/ViewController.swift
+++ GoodWeather/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
